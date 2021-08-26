ded9f6eab2f475d0b0cd00d3a8b56f7f236ba503
gagandeep singh <69230886+GaganBTS@users.noreply.github.com>
2021-08-26 14:47:48 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,8 +1,11 @@
 driver_path = r"chromedriver.exe"
 import selenium
-import time
+
+from selenium import webdriver   # for webdriver
+from selenium.webdriver.support.ui import WebDriverWait  # for implicit and explict waits
+from selenium.webdriver.chrome.options import Options  # for suppressing the browser
 from selenium.webdriver.common.keys import Keys
-from selenium import webdriver
+
 
 
 class Lyrics_Scraping:
@@ -10,11 +13,14 @@ class Lyrics_Scraping:
 
         self.lyric=[]
         self.url = 'https://www.google.com'
-        self.driver = webdriver.Chrome(executable_path=driver_path)
+        self.option = Options()
+        self.option.add_argument('--headless')
+        self.driver = webdriver.Chrome(executable_path=driver_path,options=self.option)
+
         self.driver.get(self.url)
         self.search = self.driver.find_element_by_class_name('gLFyf')
         self.search.click()
-        self.search.send_keys(f'{song} {artist} lyrics')
+        self.search.send_keys(f'{artist} {song} lyrics')
         self.search.send_keys(Keys.ENTER)
 
         self.lyrics = self.driver.find_elements_by_css_selector(("span[jsname='YS01Ge']"))
