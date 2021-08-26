928dbefcb98e1fa68475e8a63a32c35344db2c3b
gagandeep singh <69230886+GaganBTS@users.noreply.github.com>
2021-08-26 14:54:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
+driver_path = r"chromedriver.exe"
 import argparse
-
-
 import pywebio
 from pywebio.input import input, FLOAT,TEXT
 from pywebio.output import put_text,put_error,put_warning,put_loading,put_html
-from scrape import Lyrics_Scraping
+import selenium
+from selenium import webdriver   # for webdriver
+from selenium.webdriver.support.ui import WebDriverWait  # for implicit and explict waits
+from selenium.webdriver.chrome.options import Options  # for suppressing the browser
+from selenium.webdriver.common.keys import Keys
+
+
+
+class Lyrics_Scraping:
+    def __init__(self,song,artist):
+
+        self.lyric=[]
+        self.url = 'https://www.google.com'
+        self.option = Options()
+        self.option.add_argument('--headless')
+        self.driver = webdriver.Chrome(executable_path=driver_path,options=self.option)
+
+        self.driver.get(self.url)
+        self.search = self.driver.find_element_by_class_name('gLFyf')
+        self.search.click()
+        self.search.send_keys(f'{artist} {song} lyrics')
+        self.search.send_keys(Keys.ENTER)
+
+        self.lyrics = self.driver.find_elements_by_css_selector(("span[jsname='YS01Ge']"))
+
+        for l in self.lyrics:
+            self.lyric.append(l.text)
+        self.driver.quit()
+
+    def get_lyrics(self):
+
+      return self.lyric
+
+
+
+
 def lyrics():
     pywebio.session.set_env(title='LYRICS GENERATOR',output_animation=True)
     song = input('Enter the name of the song',type=TEXT)
@@ -15,7 +49,7 @@ def lyrics():
       song_lyric = Lyrics_Scraping(song,artist)
       full_lyrics = song_lyric.get_lyrics()
       if len(full_lyrics) != 0:
-       
+
        put_html(f'<h2 style="text-align:center">{song.title()} by {artist.title()} Lyrics</h2>')
        for l in full_lyrics[3:]:
         put_html(f"<h3 style='text-align:center; color:#511281'>{l}\n</h3>")
