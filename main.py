6219d0fe992d250d39595e79f0108caab7a99b49
gagandeep singh <69230886+GaganBTS@users.noreply.github.com>
2021-08-26 14:47:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
+import argparse
+
 
 import pywebio
 from pywebio.input import input, FLOAT,TEXT
@@ -8,16 +9,18 @@ def lyrics():
     pywebio.session.set_env(title='LYRICS GENERATOR',output_animation=True)
     song = input('Enter the name of the song',type=TEXT)
     artist = input('Enter the artist name',type=TEXT)
-    with put_loading('grow','dark'):
+    with put_loading('border','dark'):
 
      if song != '' or artist != '':
       song_lyric = Lyrics_Scraping(song,artist)
       full_lyrics = song_lyric.get_lyrics()
       if len(full_lyrics) != 0:
+       
+       put_html(f'<h2 style="text-align:center">{song.title()} by {artist.title()} Lyrics</h2>')
        for l in full_lyrics[3:]:
-        put_html(f"<h3>{l}\n</h3>")
+        put_html(f"<h3 style='text-align:center; color:#511281'>{l}\n</h3>")
       else:
-         put_error('Please enter the details correctly.')
+         put_error('Please enter the details correctly. Cannot find the lyrics of entered song')
      else:
         put_error('Please do not leave any field empty')
 
